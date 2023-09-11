ac992a04d26302a3d67b07c4378babe0b635e5e4
Shreyansh Bhadoria <shreyanshbhadoria@gmail.com>
2023-09-11 21:57:38 +0530

    Middleware Added

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,56 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const ProfilePage = () => {
+  const router = useRouter();
+
+  const [data, setData] = useState("nothing");
+
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      console.log("Logged Out");
+      router.push("/login");
+    } catch (error) {
+      console.log(error);
+      // toast
+    }
+  };
+
+  const getUserDetails = async () => {
+    const res = await axios.get("/api/users/me");
+    console.log(res.data);
+    setData(res.data.data._id);
+  };
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile</h1>
       <hr />
-      <p>Profile Page</p>
+      <h2 className="bg-green-500 hover:bg-green-600 p-2 text-white font-bold rounded-lg">
+        {data === "nothing" ? (
+          "Nothing"
+        ) : (
+          <Link href={`/profile/${data}`}>{data}</Link>
+        )}
+      </h2>
+      <hr />
+      <button
+        className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+        onClick={logout}
+      >
+        Logout
+      </button>
+      <button
+        className="mt-4 bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+        onClick={getUserDetails}
+      >
+        getUserDetails
+      </button>
     </div>
   );
 };
